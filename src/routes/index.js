0e80d2b1e6133111fa7de65850b680422c30c88b
matthaw <hardway3@protonmail.com>
2020-10-31 17:49:18 -0300

    feat: add cart routes to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,15 @@ const HomeController = require('../app/controllers/HomeController');
 
 const users = require('./users');
 const products = require('./products');
+const cart = require('./cart');
+const orders = require('./orders');
 
 routes.get('/', HomeController.index);
 
 routes.use('/products', products);
 routes.use('/users', users);
+routes.use('/cart', cart);
+routes.use('/orders', orders);
 
 // Alias
 routes.get('/ads/create', (request, response) => {
